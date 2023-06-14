57b7e983c5ba8d53bd1b315cb17a174b41c8ecb5
luuguas <69027878+luuguas@users.noreply.github.com>
2023-06-14 23:20:39 +0900

    Update code.js - add getValue

--- code.js
+++ code.js
@@ -21,7 +21,7 @@ let IndexedDBManager = function (databaseName, storesInfo, version = 1) {
     this.version = version;
 };
 IndexedDBManager.prototype = {
-    openDB: function () {
+    openDatabase: function () {
         return new Promise((resolve, reject) => {
             if (this.database !== null) {
                 resolve(null);
@@ -50,16 +50,48 @@ IndexedDBManager.prototype = {
                 resolve(null);
             };
         });
-    },   
+    },
+    getValue: function (storeName, key) {
+        return new Promise(async (resolve, reject) => {
+            if(this.loadingError){
+                reject('Failed to get database.');
+                return;
+            }
+            if(this.database === null){
+                try {
+                    await this.openDatabase();
+                }
+                catch (err) {
+                    reject(err);
+                    return;
+                }
+            }
+            
+            let trans = this.database.transaction(storeName, 'readonly');
+            let store = trans.objectStore(storeName);
+            let getRequest = store.get(key);
+            getRequest.onerror = (event) => {
+                reject('Failed to get value.');
+            };
+            getRequest.onsuccess = (event) => {
+                resolve(event.target.result);
+            };
+        });
+    },
 };
 
 ///////////////
 
 let myDatabase = new IndexedDBManager('myDatabase', [{storeName: 'myStore', keyPath: 'key'}]);
-myDatabase.openDB()
-.then(() => {
-    console.log('Succeeded!');
-})
-.catch((err) => {
-    console.warn('Failed... (Error: ' + err + ')');
-});
+let asyncTasks = async () => {
+    try {
+        await myDatabase.openDatabase();
+        let res = await myDatabase.getValue('myStore', 'key1');
+        console.log('Succeeded: ' + res);
+    }
+    catch (err) {
+        console.warn('Failed: ' + err);
+    }
+};
+
+asyncTasks();
