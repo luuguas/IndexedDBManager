ba016aa8c58aa5858edd0d0d7c0c7db7dcdb41df
luuguas <69027878+luuguas@users.noreply.github.com>
2023-06-16 20:57:36 +0900

    Update code.js - add getAllMatchedData

--- code.js
+++ code.js
@@ -51,7 +51,7 @@ IndexedDBManager.prototype = {
             };
             openRequest.onerror = (event) => {
                 this.loadingError = true;
-                reject('Failed to get database.');
+                reject(`Failed to get database. (${event.target.error})`);
             };
             openRequest.onsuccess = (event) => {
                 this.database = event.target.result;
@@ -61,10 +61,10 @@ IndexedDBManager.prototype = {
         });
     },
     /**
-     * データベースからデータを入手する非同期関数
+     * ストアからデータを入手する非同期関数
      * @param {string} storeName ストア名
      * @param {string} key 入手するデータのキー名
-     * @return {Promise<?object>} オブジェクト型のデータを返す(該当するデータがなければundefinedを返す)
+     * @return {Promise<?object>} データを返す(該当するデータがなければnullを返す)
      */
     getData(storeName, key) {
         return new Promise(async (resolve, reject) => {
@@ -79,15 +79,56 @@ IndexedDBManager.prototype = {
             let trans = this.database.transaction(storeName, 'readonly');
             let getRequest = trans.objectStore(storeName).get(key);
             getRequest.onerror = (event) => {
-                reject('Failed to get data.');
+                reject(`Failed to get data. (${event.target.error})`);
             };
             getRequest.onsuccess = (event) => {
-                resolve(event.target.result);
+                if(event.target.result) {
+                    resolve(event.target.result);
+                }
+                else {
+                    resolve(null);
+                }
             };
         });
     },
     /**
-     * データベースにデータを追加する(既に同じキーのデータがあれば上書きする)非同期関数
+     * ストアから条件に一致するデータ全てを入手する非同期関数
+     * @param {string} storeName ストア名
+     * @param {(data: object) => boolean} filter 引数に与えられたデータが条件に一致する場合にtrueを返すコールバック関数
+     * @returns {Promise<object[]>} 条件に一致するデータを要素に持つ配列を返す
+     */
+    getAllMatchedData(storeName, filter) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                await this.openDatabase(false);
+            }
+            catch (err) {
+                reject(err);
+                return;
+            }
+            
+            let trans = this.database.transaction(storeName, 'readonly');
+            let cursorRequest = trans.objectStore(storeName).openCursor();
+            let res = [];
+            cursorRequest.onerror = (event) => {
+                reject(`Failed to get cursor. (${event.target.error})`);
+            };
+            cursorRequest.onsuccess = (event) => {
+                let cursor = event.target.result;
+                if(cursor) {
+                    if(filter(cursor.value)) {
+                        res.push(cursor.value);
+                    }
+                    cursor.continue();
+                }
+                else {
+                    resolve(res);
+                }
+            };
+        });
+    },
+    /**
+     * ストアにデータを追加する非同期関数 既に同じキーを持つデータがあればそれを上書きする
      * @param {string} storeName ストア名
      * @param {object} data 追加するデータ(keyPathで指定したキーのプロパティを持つ)
      * @return {Promise<null>}
@@ -105,7 +146,7 @@ IndexedDBManager.prototype = {
             let trans = this.database.transaction(storeName, 'readwrite');
             let getRequest = trans.objectStore(storeName).put(data);
             getRequest.onerror = (event) => {
-                reject('Failed to set data.');
+                reject(`Failed to set data. (${event.target.error})`);
             };
             getRequest.onsuccess = (event) => {
                 resolve(null);
@@ -113,7 +154,7 @@ IndexedDBManager.prototype = {
         });
     },
     /**
-     * データベースからデータを削除する非同期関数
+     * ストアからデータを削除する非同期関数
      * @param {string} storeName ストア名
      * @param {string} key 削除するデータのキー名
      * @return {Promise<null>}
@@ -131,7 +172,7 @@ IndexedDBManager.prototype = {
             let trans = this.database.transaction(storeName, 'readwrite');
             let getRequest = trans.objectStore(storeName).delete(key);
             getRequest.onerror = (event) => {
-                reject('Failed to delete data.');
+                reject(`Failed to delete data. (${event.target.error})`);
             };
             getRequest.onsuccess = (event) => {
                 resolve(null);
@@ -144,19 +185,20 @@ IndexedDBManager.prototype = {
 
 let myDatabase = new IndexedDBManager('myDatabase', [{storeName: 'myStore', keyPath: 'key'}]);
 let asyncTasks = async () => {
-    let d = new Date();
-    let key = d.getSeconds() % 10;
-    let value = d.toLocaleString();
+    let value = new Date();
+    let key = value.getSeconds() % 10;
     try {
         await myDatabase.openDatabase();
         let oldData = await myDatabase.getData('myStore', key);
         await myDatabase.setData('myStore', {key, value});
         let data = await myDatabase.getData('myStore', key);
-        console.log(`Succeeded: data = {key: ${data.key}, value: "${data.value}}"`);
+        console.log(`Succeeded: data = {key: ${data.key}, value: ${data.value.toLocaleString()}}`);
         if(oldData) {
-            console.log(`Info: oldData = {key: ${oldData.key}, value: "${oldData.value}}"`);
+            console.log(`Info: oldData = {key: ${oldData.key}, value: ${oldData.value.toLocaleString()}}`);
         }
-        await myDatabase.deleteData('myStore', (key + 1) % 10);
+
+        let matchedData = await myDatabase.getAllMatchedData('myStore', (data) => {return (data.value.getMinutes() % 10) >= 5;});
+        console.log(matchedData);
     }
     catch (err) {
         console.warn(`Failed: ${err}`);
