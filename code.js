7248c1d1bf8dd9ce86ea45faa07f79ff0c12b68a
luuguas <69027878+luuguas@users.noreply.github.com>
2023-12-24 15:09:50 +0900

    Update code.js - change to class / add openDatabase

--- code.js
+++ code.js
@@ -1,246 +1,107 @@
-/**
- * IndexedDBの処理をまとめたクラス
- * @constructor
- * @param {string} databaseName データベース名
- */
-let IDBManager = function (databaseName) {
-    this.database = null;
-    this.databaseName = databaseName;
-};
-IDBManager.prototype = {
-    /**
-     * @typedef {object} storeInfosType オブジェクトストアの情報をまとめたオブジェクト
-     * @property {string} storeName ストア名
-     * @property {string} keyPath データを区別するためのキー名
-     */
-    /**
-     * データベースを開く非同期関数
-     * データベースが新規作成もしくはバージョンが更新された場合のみ、追加でストアの作成・削除をおこなう
-     * データベースに存在せずstoreInfosに含まれるストアを作成し、データベースに存在してstoreInfosに含まれないストアを削除する
-     * @param {storeInfosType[]} storeInfos オブジェクトストアの情報
-     * @param {version} データベースのバージョン
-     * @return {Promise<null>}
-     */
-    openDatabase(storeInfos, version) {
-        return new Promise((resolve, reject) => {
-            if (this.database !== null) {
-                resolve(null);
-                return;
-            }
-            if (typeof window.indexedDB === 'undefined') {
-                reject('IndexedDB is not supported.');
-                return;
-            }
+// ==UserScript==
+// @name         IDBManagerTest
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://example.com*
+// @grant        none
+// ==/UserScript==
 
-            let openRequest = window.indexedDB.open(this.databaseName, version);
-            openRequest.onupgradeneeded = (event) => {
-                let database = event.target.result;
-                let m = new Map();
-                for (let name of database.objectStoreNames) {
-                    m.set(name, { status: 1, keyPath: null });
-                }
-                for (let info of storeInfos) {
-                    if (m.get(info.storeName)) {
-                        m.set(info.storeName, { status: 2, keyPath: info.keyPath });
-                    }
-                    else {
-                        m.set(info.storeName, { status: 0, keyPath: info.keyPath });
-                    }
-                }
-                for (let [name, info] of m) {
-                    if (info.status === 0) {
-                        database.createObjectStore(name, { keyPath: info.keyPath });
-                    }
-                    else if (info.status === 1) {
-                        database.deleteObjectStore(name);
-                    }
-                }
-                console.info('Database was created or upgraded.');
-            };
-            openRequest.onerror = (event) => {
-                this.database = null;
-                reject(`Failed to get database. (${event.target.error})`);
-            };
-            openRequest.onsuccess = (event) => {
-                this.database = event.target.result;
-                resolve(null);
-            };
-        });
-    },
-    /**
-     * データベースが開かれているかを返す関数
-     * @return {boolean} データベースが開かれているならtrue、そうでないならfalseを返す
-     */
-    isOpened() {
-        return this.database !== null;
-    },
-    /**
-     * ストアからデータを入手する非同期関数
-     * @param {string} storeName ストア名
-     * @param {string} key 入手するデータのキー
-     * @return {Promise<?object>} オブジェクト型のデータを返す 該当するデータがなければnullを返す
-     */
-    getData(storeName, key) {
-        return new Promise((resolve, reject) => {
-            if (!this.isOpened()) {
-                reject('Database is not loaded.');
-                return;
-            }
-            
-            let trans = this.database.transaction(storeName, 'readonly');
-            let getRequest = trans.objectStore(storeName).get(key);
-            getRequest.onerror = (event) => {
-                reject(`Failed to get data. (${event.target.error})`);
-            };
-            getRequest.onsuccess = (event) => {
-                if (event.target.result) {
-                    resolve(event.target.result);
-                }
-                else {
-                    resolve(null);
-                }
-            };
-        });
-    },
-    /**
-     * ストアから条件に一致するデータ全てを入手する非同期関数
-     * @param {string} storeName ストア名
-     * @param {(data: object) => boolean} filter 引数に与えられたデータが条件に一致する場合にtrueを返すコールバック関数
-     * @return {Promise<object[]>} 条件に一致するデータを要素に持つ配列を返す
-     */
-    getAllMatchedData(storeName, filter) {
-        return new Promise((resolve, reject) => {
-            if (!this.isOpened()) {
-                reject('Database is not loaded.');
-                return;
-            }
-            
-            let trans = this.database.transaction(storeName, 'readonly');
-            let cursorRequest = trans.objectStore(storeName).openCursor();
-            let res = [];
-            cursorRequest.onerror = (event) => {
-                reject(`Failed to get cursor. (${event.target.error})`);
-            };
-            cursorRequest.onsuccess = (event) => {
-                let cursor = event.target.result;
-                if (cursor) {
-                    if (filter(cursor.value)) {
-                        res.push(cursor.value);
-                    }
-                    cursor.continue();
-                }
-                else {
-                    resolve(res);
-                }
-            };
-        });
-    },
-    /**
-     * ストア内のデータの個数を返す非同期関数
-     * @param {string} storeName ストア名
-     * @return {Promise<number>} データの個数を返す
-     */
-    countData(storeName) {
-        return new Promise((resolve, reject) => {
-            if (!this.isOpened()) {
-                reject('Database is not loaded.');
-                return;
-            }
-            
-            let trans = this.database.transaction(storeName, 'readonly');
-            let countRequest = trans.objectStore(storeName).count();
-            countRequest.onerror = (event) => {
-                reject(`Failed to count data. (${event.target.error})`);
-            };
-            countRequest.onsuccess = (event) => {
-                resolve(event.target.result);
-            };
-        });
-    },
-    /**
-     * ストアにデータを追加する非同期関数 既に同じキーを持つデータがあれば上書きする
-     * @param {string} storeName ストア名
-     * @param {object} data 追加するデータ keyPathで指定したキーのプロパティを持つ
-     * @return {Promise<null>}
-     */
-    setData(storeName, data) {
-        return new Promise((resolve, reject) => {
-            if (!this.isOpened()) {
-                reject('Database is not loaded.');
-                return;
-            }
-            
-            let trans = this.database.transaction(storeName, 'readwrite');
-            let setRequest = trans.objectStore(storeName).put(data);
-            setRequest.onerror = (event) => {
-                reject(`Failed to set data. (${event.target.error})`);
-            };
-            setRequest.onsuccess = (event) => {
-                resolve(null);
-            };
-        });
-    },
-    /**
-     * ストアからデータを削除する非同期関数
-     * @param {string} storeName ストア名
-     * @param {string} key 削除するデータのキー
-     * @return {Promise<null>}
-     */
-    deleteData(storeName, key) {
-        return new Promise((resolve, reject) => {
-            if (!this.isOpened()) {
-                reject('Database is not loaded.');
-                return;
-            }
-            
-            let trans = this.database.transaction(storeName, 'readwrite');
-            let deleteRequest = trans.objectStore(storeName).delete(key);
-            deleteRequest.onerror = (event) => {
-                reject(`Failed to delete data. (${event.target.error})`);
-            };
-            deleteRequest.onsuccess = (event) => {
-                resolve(null);
-            };
-        });
-    },
-    /**
-     * ストアから全てのデータを削除する非同期関数
-     * @param {string} storeName ストア名
-     * @return {Promise<null>}
-     */
-    deleteAllData(storeName) {
-        return new Promise((resolve, reject) => {
-            if (!this.isOpened()) {
-                reject('Database is not loaded.');
-                return;
-            }
-            
-            let trans = this.database.transaction(storeName, 'readwrite');
-            let deleteRequest = trans.objectStore(storeName).clear();
-            deleteRequest.onerror = (event) => {
-                reject(`Failed to delete all data. (${event.target.error})`);
-            };
-            deleteRequest.onsuccess = (event) => {
-                resolve(null);
-            };
-        });
-    },
-};
+'use strict';
 
-/**
- * データベースを削除する非同期関数
- * @param {string} データベース名
- * @return {Promise<null>}
- */
-function deleteDatabase(databaseName) {
-    return new Promise((resolve, reject) => {
-        let deleteRequest = window.indexedDB.deleteDatabase(databaseName);
-        deleteRequest.onerror = (event) => {
-            reject(`Failed to delete database. (${event.target.error})`);
-        };
-        deleteRequest.onsuccess = (event) => {
-            console.info('Database was deleted.');
-            resolve(null);
-        };
-    });
+class IDBManager {
+    #db;
+    #storeUpdateType = Object.freeze({ 'remain': 0, 'new': 1, 'delete': 2, 'recreate': 3 });
+    #storeOptions = Object.freeze([ 'keyPath', 'autoIncrement' ]);
+    #indexOptions = Object.freeze([ 'unique', 'multiEntry', 'locale' ]);
+    
+    constructor() { this.#db = null; }
+    
+    //*は省略可
+    //objectStoreInfos = [ storeInfo1, storeInfo2, ... ]
+    //storeInfo = { name, *keyPath, *autoIncrement, *indexInfos }
+    //indexInfos = [ indexInfo1, indexInfo2, ... ]
+    //indexInfo = { name, keyPath, *unique, *multiEntry }
+    openDatabase(databaseName, version, objectStoreInfos) {
+        return new Promise((resolve, reject) => {
+            const openRequest = window.indexedDB.open(databaseName, version);
+            let upgraded = false;
+            
+            openRequest.onblocked = (e) => { reject(e.target.error); };
+            openRequest.onerror = (e) => { reject(e.target.error); };
+            openRequest.onsuccess = (e) => {
+                this.db = e.target.result;
+                resolve(upgraded);
+            };
+            
+            openRequest.onupgradeneeded = (e) => {
+                upgraded = true;
+                const db = e.target.result;
+                
+                const m = new Map();
+                for (const name of db.objectStoreNames) {
+                    m.set(name, { type: this.#storeUpdateType['delete'], options: null, indexInfos: null });
+                }
+                for (const storeInfo of objectStoreInfos) {
+                    const options = {};
+                    for (const op of this.#storeOptions) {
+                        if (storeInfo.hasOwnProperty(op)) options[op] = storeInfo[op];
+                    }
+                    
+                    if (m.has(storeInfo.name)) {
+                        if (storeInfo.recreate) {
+                            m.set(storeInfo.name, { type: this.#storeUpdateType['recreate'], options, indexInfos: storeInfo.indexInfos });
+                        } else {
+                            m.set(storeInfo.name, { type: this.#storeUpdateType['remain'], options: null, indexInfos: null });
+                        }
+                    } else {
+                        m.set(storeInfo.name, { type: this.#storeUpdateType['new'], options, indexInfos: storeInfo.indexInfos });
+                    }
+                }
+                
+                for (const [name, obj] of m) {
+                    switch (obj.type) {
+                        case this.#storeUpdateType['delete']:
+                            db.deleteObjectStore(name);
+                            break;
+                        case this.#storeUpdateType['recreate']:
+                            db.deleteObjectStore(name);
+                        case this.#storeUpdateType['new']:
+                            const store = db.createObjectStore(name, obj.options);
+                            if (obj.indexInfos) {
+                                for (const indexInfo of obj.indexInfos) {
+                                    const options = {};
+                                    for (const op of this.#indexOptions) {
+                                        if (indexInfo.hasOwnProperty(op)) options[op] = indexInfo[op];
+                                    }
+                                    store.createIndex(indexInfo.name, indexInfo.keyPath, options);
+                                }
+                            }
+                            break;
+                    }
+                }
+            };
+        });
+    }
 }
+
+(function() {
+    const idb = new IDBManager();
+    const objectStoreInfos = [
+        { name: 'ObjectStore1' },
+        { name: 'ObjectStore2', keyPath: 'key' },
+        { name: 'ObjectStore3', autoIncrement: true },
+        { name: 'ObjectStore4', keyPath: 'hoge', autoIncrement: true },
+        { name: 'ObjectStore5', recreate: true, keyPath: 'name', indexInfos: [ { name: 'by_age', keyPath: 'age' }, { name: 'by_email', keyPath: 'email', unique: true } ] },
+    ];
+    
+    idb.openDatabase('MyDatabase', 1, objectStoreInfos)
+        .then((response) => {
+            console.log('success');
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log('error');
+            console.error(error);
+        });
+})();
