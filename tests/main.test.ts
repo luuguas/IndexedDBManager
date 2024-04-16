a5b531b88a3dce150e5af72a788c7a4b7baca058
luuguas <luuguas368435@gmail.com>
2024-04-17 00:50:29 +0900

    Remove main.ts & main.test.ts / Add script.ts & script.test.ts (empty files)

--- tests/main.test.ts
+++
@@ -1,8 +0,0 @@
-import { hello } from '../src/main';
-
-describe('hello function test', () => {
-    test('returning "Hello, world!"', () => {
-        const res = hello('world');
-        expect(res).toBe('Hello, world!');
-    });
-});
