4fd4c5dcab34424f991686b8b58d5656aa761b01
luuguas <luuguas368435@gmail.com>
2024-04-19 23:44:31 +0900

    Change script.ts: add openDatabase

--- src/script.ts
+++ src/script.ts
@@ -8,4 +8,19 @@ export class IDBManager {
         this.dbName = databaseName;
         this.dbVersion = version;
     }
+    openDatabase(): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            const openReq = window.indexedDB.open(this.dbName, this.dbVersion);
+
+            openReq.onerror = (e) => {
+                const target = e.target as IDBOpenDBRequest;
+                reject(target.error);
+            };
+            openReq.onsuccess = (e) => {
+                const target = e.target as IDBOpenDBRequest;
+                this.db = target.result;
+                resolve(true);
+            };
+        });
+    }
 }
