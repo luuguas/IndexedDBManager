4522df2329fd8eb32dc4ab8e71cfcc58dcdb7101
luuguas <luuguas368435@gmail.com>
2024-04-19 23:55:40 +0900

    Change script.test.ts: add openDatabase test: resolve

--- tests/script.test.ts
+++ tests/script.test.ts
@@ -1,2 +1,9 @@
 import 'fake-indexeddb/auto';
 import { IDBManager } from '../src/script';
+
+describe('openDatabaseのテスト', () => {
+    test('正常にデータベースを開く', async () => {
+        const idb = new IDBManager('MyDB', 1);
+        await expect(idb.openDatabase()).resolves.toBe(true);
+    });
+});
