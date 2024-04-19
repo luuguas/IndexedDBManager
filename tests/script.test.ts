5518050781d5ad728d8f2ba7a5bbb3c97be154ea
luuguas <luuguas368435@gmail.com>
2024-04-19 23:57:05 +0900

    Change script.test.ts: add openDatabase test: reject with TypeError

--- tests/script.test.ts
+++ tests/script.test.ts
@@ -6,4 +6,8 @@ describe('openDatabaseのテスト', () => {
         const idb = new IDBManager('MyDB', 1);
         await expect(idb.openDatabase()).resolves.toBe(true);
     });
+    test('不正なバージョンを指定した場合は失敗する', async () => {
+        const idb = new IDBManager('MyDB', 0);
+        await expect(idb.openDatabase()).rejects.toThrow(TypeError);
+    });
 });
