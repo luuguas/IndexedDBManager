b4caed750150db75d60f1366f13a5f6229dcb9ef
luuguas <luuguas368435@gmail.com>
2024-04-20 11:59:32 +0900

    Change script.test.ts: add openDatabase test: creating object store

--- tests/script.test.ts
+++ tests/script.test.ts
@@ -22,4 +22,10 @@ describe('openDatabaseのテスト', () => {
         const idb = new IDBManager(getNewDBName(), 0, []);
         await expect(idb.openDatabase()).rejects.toThrow(TypeError);
     });
+
+    test('オブジェクトストアを作成する', async () => {
+        const idb = new IDBManager(getNewDBName(), 1, [{ name: 'MyStore1' }]);
+        await expect(idb.openDatabase()).resolves.toBe(true);
+        expect(idb.objectStoreNames).toContain('MyStore1');
+    });
 });
