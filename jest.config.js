57fde0cc716936327add4f088f2cee37d9292b65
luuguas <luuguas368435@gmail.com>
2025-02-17 22:04:56 +0900

    build: テスト用ライブラリを追加
    fake-indexeddb, jest-environment-jsdomを追加し、Jestのテスト環境をjsdomに変更

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>/tests'],
   collectCoverage: true,
   collectCoverageFrom: [
