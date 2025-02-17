b4fc3ec24a61995a6188608780535df7be47cac9
luuguas <luuguas368435@gmail.com>
2025-02-17 21:29:19 +0900

    add: テンプレートをTypeScriptTemplateRepositoryからコピー
    https://github.com/luuguas/TypeScriptTemplateRepository.git

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ['dist', 'node_modules', 'coverage'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 4],
+    '@typescript-eslint/typedef': ['error', { memberVariableDeclaration: true }],
+    'lines-between-class-members': 'off',
+    '@typescript-eslint/lines-between-class-members': 'off',
+    'arrow-body-style': ['error', 'always'],
+  },
+  overrides: [
+    {
+      files: ['.*.js', '*.js'],
+      rules: {
+        '@typescript-eslint/indent': ['error', 2],
+      },
+    },
+  ],
+};
