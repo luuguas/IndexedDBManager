8db364e017a6dcf349ec8a172387e835a2cd4e33
luuguas <luuguas368435@gmail.com>
2025-02-18 22:11:53 +0900

    add: openDatabase, closeDatabaseを追加

--- src/script.ts
+++ src/script.ts
@@ -8,4 +8,23 @@ export class IDBManager {
         this.dbName = dbName;
         this.dbVersion = dbVersion;
     }
+
+    openDatabase(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (this.db !== null) { return; }
+
+            const openReq = window.indexedDB.open(this.dbName, this.dbVersion);
+
+            openReq.onerror = (e) => { reject(openReq.error); };
+            openReq.onsuccess = (e) => {
+                this.db = openReq.result;
+                resolve();
+            };
+        });
+    }
+
+    closeDatabase(): void {
+        this.db?.close();
+        this.db = null;
+    }
 }
