c1fcee6f39a7f3f520ad0870ed98bdc1cbd97478
luuguas <luuguas368435@gmail.com>
2025-02-20 01:43:10 +0900

    test: DBの開閉テスト(オブジェクトストアなし)を追加
    openDatabase, closeDatabase, isOpen, isCloseをテスト

---
+++ tests/script.test.ts
@@ -0,0 +1,50 @@
+import 'fake-indexeddb/auto';
+import { IDBManager } from '../src/script';
+
+// データベース名を連番で生成するクロージャ
+function dbNameGenerator(prefix: string, digits: number): () => string {
+    let count = 1;
+    return () => {
+        const dbName = `${prefix}${(count).toString().padStart(digits)}`;
+        count += 1;
+        return dbName;
+    };
+}
+
+const createDBName: () => string = dbNameGenerator('MyDB', 3);
+
+describe('DBの開閉テスト(オブジェクトストアなし)', () => {
+    const dbName = createDBName();
+
+    test('DBを開く前', () => {
+        const idb = new IDBManager(dbName, 1);
+
+        expect(idb.isOpen()).toBe(false);
+        expect(idb.isClose()).toBe(true);
+    });
+    test('DBを正常に開いて閉じる', async () => {
+        const idb = new IDBManager(dbName, 1);
+
+        await expect(idb.openDatabase()).resolves.toBeUndefined();
+        expect(idb.isOpen()).toBe(true);
+        expect(idb.isClose()).toBe(false);
+
+        expect(idb.closeDatabase()).toBeUndefined();
+        expect(idb.isOpen()).toBe(false);
+        expect(idb.isClose()).toBe(true);
+    });
+
+    test('DBを連続で開く/閉じる', async () => {
+        const idb = new IDBManager(dbName, 1);
+
+        await expect(idb.openDatabase()).resolves.toBeUndefined();
+        await expect(idb.openDatabase()).resolves.toBeUndefined();
+        expect(idb.closeDatabase()).toBeUndefined();
+        expect(idb.closeDatabase()).toBeUndefined();
+    });
+    test('不正なバージョンを指定するとDBを開けない', async () => {
+        const idb = new IDBManager(createDBName(), 0);
+
+        await expect(idb.openDatabase()).rejects.toThrow(TypeError);
+    });
+});
