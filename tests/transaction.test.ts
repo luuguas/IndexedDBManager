523eac45a6c4a00b654d8d116a16b3bcf76b2759
luuguas <luuguas368435@gmail.com>
2026-06-05 10:40:00 +0900

    test: tx.addItemのテストを追加
    発火関係メモは後で消すかも

--- tests/transaction.test.ts
+++ tests/transaction.test.ts
@@ -87,5 +87,63 @@ describe('CRUD共通の例外処理', () => {
     });
 
     test('存在しないオブジェクトストアを指定するとエラー', async () => {
+        let tx: IDBMTransaction;
+
+        // eslint-disable-next-line prefer-const
+        tx = new IDBMTransaction(pidb.p_db, 'MyStore1', 'readwrite');
+        await expect(tx.addItem('MyStoreX', 'Apple', 'A')).rejects.toThrow(DOMException);
+        // settlementもrejectされることをexpectで確認する これをしないとテストが落ちる
+        await expect(tx.getSettlement()).rejects.toThrow(DOMException);
     });
 });
+
+describe('addItemのテスト', () => {
+    const pidb = new PublicIDBManager(dbName, 1, storeInfos);
+
+    beforeEach(async () => {
+        await pidb.openDatabase();
+    });
+    afterEach(() => {
+        pidb.closeDatabase();
+    });
+
+    test('アイテムを追加する', async () => {
+        const tx = new IDBMTransaction(pidb.p_db, 'MyStore1', 'readwrite');
+        await expect(tx.addItem('MyStore1', 'Apple', 'A')).resolves.toBe('A');
+        await expect(tx.getSettlement()).resolves.toBeUndefined();
+        await expect(pidb.getItem('MyStore1', 'A')).resolves.toBe('Apple');
+    });
+
+    test('キー指定を間違えるとabort', async () => {
+        const tx = new IDBMTransaction(pidb.p_db, 'MyStore1', 'readwrite');
+        await expect(tx.addItem('MyStore1', 'Banana', 'B')).resolves.toBe('B');
+        // addReq.add() が直接例外を投げる addReq.onerror は発火しない
+        await expect(tx.addItem('MyStore1', 'Cherry', undefined)).rejects.toThrow(DOMException);
+
+        await expect(tx.getSettlement()).rejects.toThrow(DOMException);
+        // トランザクションがabortされているため、データは追加されていない
+        await expect(pidb.getItem('MyStore1', 'B')).resolves.toBeUndefined();
+    });
+    test('既に存在するキーを指定するとエラー', async () => {
+        const tx = new IDBMTransaction(pidb.p_db, 'MyStore1', 'readwrite');
+        await expect(tx.addItem('MyStore1', 'Banana', 'B')).resolves.toBe('B');
+        // addReq.onerror が発火する
+        await expect(tx.addItem('MyStore1', 'Bread', 'B')).rejects.toThrow(DOMException);
+
+        await expect(tx.getSettlement()).rejects.toThrow(DOMException);
+        // トランザクションがabortされているため、データは追加されていない
+        await expect(pidb.getItem('MyStore1', 'B')).resolves.toBeUndefined();
+    });
+});
+
+/*
+tx.onabort と tx.onerror の発火関係メモ
+Case1: tx.abort() した場合
+    tx.onabort のみ発火(undefined)
+Case2: const addReq = tx.store.add() で既に存在するキーを指定した場合
+    addReq.onerror が発火(ConstraintError) -> tx.onerror が発火(undefined)
+    -> tx.onabort(ConstraintError) が発火
+Case3: const addReq = tx.store.add() でキー指定を間違えたとき
+    tx.store.add() が直接例外を投げるので、addReq.onerror は発火せず、tx.onerror および tx.onabort も発火しない
+    ゆえにtx.abort()してあげる必要がある
+*/
