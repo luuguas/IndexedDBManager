20d8029faa10284e2576b0262f62c966b90602af
luuguas <luuguas368435@gmail.com>
2026-06-06 18:34:03 +0900

    add: トランザクションに追加・更新・削除関数を追加

--- src/transaction.ts
+++ src/transaction.ts
@@ -122,4 +122,164 @@ export class IDBMTransaction {
             }
         });
     }
+
+    setItem<TItem>(
+        storeName: string,
+        item: TItem,
+        key?: IDBValidKey,
+    ): Promise<IDBValidKey> {
+        return new Promise((resolve, reject) => {
+            if (!this.isActive()) {
+                reject(IDBMTransaction.txNotActiveError());
+                return;
+            }
+
+            try {
+                const store = this.tx.objectStore(storeName);
+                const putReq = store.put(item, key);
+                putReq.onerror = () => {
+                    if (this.isActive()) { this.abort(putReq.error); }
+                    reject(putReq.error);
+                };
+                putReq.onsuccess = () => { resolve(putReq.result); };
+            }
+            catch (error) {
+                if (this.isActive()) {
+                    if (error instanceof Error) { this.abort(error); }
+                    else { this.abort(); }
+                }
+                reject(error);
+            }
+        });
+    }
+
+    setItems<TItem>(
+        storeName: string,
+        items: TItem[],
+        keys?: (IDBValidKey | undefined)[],
+    ): Promise<IDBValidKey[]> {
+        return new Promise((resolve, reject) => {
+            if (!this.isActive()) {
+                reject(IDBMTransaction.txNotActiveError());
+                return;
+            }
+            if (keys instanceof Array && items.length !== keys.length) {
+                const error = new TypeError('The length of items and keys must be the same.');
+                if (this.isActive()) { this.abort(error); }
+                reject(error);
+                return;
+            }
+
+            try {
+                const store = this.tx.objectStore(storeName);
+                const promises: Promise<IDBValidKey>[] = items.map((item, idx) => {
+                    return new Promise((res, rej) => {
+                        const putReq = store.put(item, keys?.[idx]);
+                        putReq.onerror = () => { rej(putReq.error); };
+                        putReq.onsuccess = () => { res(putReq.result); };
+                    });
+                });
+
+                Promise.all(promises)
+                    .then((response: IDBValidKey[]) => { resolve(response); })
+                    .catch((error: DOMException) => {
+                        if (this.isActive()) { this.abort(error); }
+                        reject(error);
+                    });
+            }
+            catch (error) {
+                if (this.isActive()) {
+                    if (error instanceof Error) { this.abort(error); }
+                    else { this.abort(); }
+                }
+                reject(error);
+            }
+        });
+    }
+
+    removeItem(storeName: string, key: IDBValidKey): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (!this.isActive()) {
+                reject(IDBMTransaction.txNotActiveError());
+                return;
+            }
+
+            try {
+                const store = this.tx.objectStore(storeName);
+                const deleteReq = store.delete(key);
+                deleteReq.onerror = () => {
+                    if (this.isActive()) { this.abort(deleteReq.error); }
+                    reject(deleteReq.error);
+                };
+                deleteReq.onsuccess = () => { resolve(); };
+            }
+            catch (error) {
+                if (this.isActive()) {
+                    if (error instanceof Error) { this.abort(error); }
+                    else { this.abort(); }
+                }
+                reject(error);
+            }
+        });
+    }
+
+    removeItems(storeName: string, keys: IDBValidKey[]): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (!this.isActive()) {
+                reject(IDBMTransaction.txNotActiveError());
+                return;
+            }
+
+            try {
+                const store = this.tx.objectStore(storeName);
+                const promises: Promise<void>[] = keys.map((key) => {
+                    return new Promise((res, rej) => {
+                        const deleteReq = store.delete(key);
+                        deleteReq.onerror = () => { rej(deleteReq.error); };
+                        deleteReq.onsuccess = () => { res(); };
+                    });
+                });
+
+                Promise.all(promises)
+                    .then(() => { resolve(); })
+                    .catch((error: DOMException) => {
+                        if (this.isActive()) { this.abort(error); }
+                        reject(error);
+                    });
+            }
+            catch (error) {
+                if (this.isActive()) {
+                    if (error instanceof Error) { this.abort(error); }
+                    else { this.abort(); }
+                }
+                reject(error);
+            }
+        });
+    }
+
+    clearItems(storeName: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (!this.isActive()) {
+                reject(IDBMTransaction.txNotActiveError());
+                return;
+            }
+
+            try {
+                const store = this.tx.objectStore(storeName);
+                const clearReq = store.clear();
+                clearReq.onerror = () => {
+                    if (this.isActive()) { this.abort(clearReq.error); }
+                    reject(clearReq.error);
+                };
+                clearReq.onsuccess = () => { resolve(); };
+            }
+            catch (error) {
+                if (this.isActive()) {
+                    if (error instanceof Error) { this.abort(error); }
+                    else { this.abort(); }
+                }
+                reject(error);
+            }
+        });
+    }
 }
