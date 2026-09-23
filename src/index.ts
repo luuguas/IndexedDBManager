294282bf903216ecb2305cc689ae38eebe6d77fb
luuguas <luuguas368435@gmail.com>
2026-09-23 22:15:34 +0900

    add: index.tsを追加
    複数のモジュールを1個のファイルにまとめてexport

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { IDBMTransaction } from './transaction';
+export { IDBManager } from './manager';
+
+export type { IDBMKeyRange } from './transaction';
+export type { IDBMIndexInfo, IDBMStoreInfo } from './manager';
